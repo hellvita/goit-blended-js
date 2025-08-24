9530f8df5d5f70e2cec34ccf52cda4c853b36ce5
hellvita <seredaolga03@gmail.com>
2025-08-24 16:00:43 +0200

    add the task-4 script

---
+++ js/task-4.js
@@ -0,0 +1,14 @@
+const time = Number(prompt("Enter time in minutes"));
+
+const hours =
+  (time - (time % 60)) / 60 < 10
+    ? "0" + (time - (time % 60)) / 60
+    : (time - (time % 60)) / 60;
+const min =
+  time % 60 < 10 ? `0${Math.floor(time % 60)}` : Math.floor(time % 60);
+const result =
+  time < 1440 && typeof time === "number"
+    ? `${hours}:${min}`
+    : "Sorry,  you entered invalid data";
+
+console.log("result: ", result);
