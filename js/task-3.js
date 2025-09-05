1efbe1bc3747cf28a2e976081f3a38dbf159107d
hellvita <seredaolga03@gmail.com>
2025-09-05 15:09:01 +0200

    refactor the task-3 code

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
-const calculateAverage = (numbers) => {
+const calculateAverage = (...numbers) => {
   let result = "Numbers only allowed!";
-  if (!numbers || numbers.length === 0) {
+
+  if (numbers.length === 0 || numbers.every((number) => number.length === 0)) {
     result = "Nothing to calculate!";
   } else if (numbers.every((number) => typeof number === "number")) {
     result =
@@ -8,17 +9,21 @@ const calculateAverage = (numbers) => {
         return (sum += number);
       }, 0) / numbers.length;
   }
+
   return result;
 };
 
-console.log("calculateAverage([5, 15]): ", calculateAverage([5, 15]));
-console.log("calculateAverage(): ", calculateAverage());
-console.log("calculateAverage([]): ", calculateAverage([]));
 console.log(
-  "calculateAverage(['a', 'b', 'c']): ",
-  calculateAverage(["a", "b", "c"])
+  'calculateAverage("a", "b", "c"): ',
+  calculateAverage("a", "b", "c"),
+  "\n"
 );
+console.log("calculateAverage(1, 2, 3): ", calculateAverage(1, 2, 3), "\n");
+console.log("calculateAverage(): ", calculateAverage(), "\n");
+console.log("calculateAverage([]): ", calculateAverage([]), "\n");
 console.log(
-  "calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]): ",
-  calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  'calculateAverage(2, 10, [], "c", 13): ',
+  calculateAverage(2, 10, [], "c", 13),
+  "\n"
 );
+console.log("calculateAverage(50, 50): ", calculateAverage(50, 50), "\n");
